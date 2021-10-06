cb40f2398c1c5b68dd11ae966839d283ac5e167c
Prashant Ranjan <ranjan.prashant3655@gmail.com>
2021-10-06 15:51:14 +0530

    second commit

--- code.cpp
+++ code.cpp
@@ -1,9 +1,58 @@
+// graph repesentation without edgeweight in undirected graph using adjencency matrix
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    int n=5;
-    for(int i=0;i<n;i++){
-        cout<<i+1<<"\n";
+    int noOfNode,noOfEdges;
+    cin>>noOfNode>>noOfEdges;
+    vector<vector<int>>graph(noOfNode+1, vector<int>(noOfEdges+1,0));
+    while(noOfEdges--){
+        int u,v,w;
+        cin>>u>>v;
+        graph[u][v]=1;
+        graph[v][u]=1;
+
     }
+    return 0;
+}
+
+
+
+// graph repesentation with edgeweight in undirected graph using adjencency list
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int noOfNode,noOfEdges;
+    cin>>noOfNode>>noOfEdges;
+    vector<pair<int,int>>graph[noOfNode];
+    while(noOfEdges--){
+        int u,v,w;
+        cin>>u>>v>>w;
+        graph[u].push_back({v,w});
+        graph[v].push_back({u,w});
+
+    }
+    
+}
+
+
+// graph repesentation with edgeweight in directed graph using adjencency list
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int noOfNode,noOfEdges;
+    cin>>noOfNode>>noOfEdges;
+    vector<pair<int,int>>graph[noOfNode];
+    while(noOfEdges--){
+        int u,v,w;
+        cin>>u>>v>>w;
+        graph[u].push_back({v,w});
+    }
+    return 0;
 }
